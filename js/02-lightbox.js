06e91e067a780ae9a6c38ba4d9013e6996112e05
Svitlana Samoliuk <101995368+Svetlana1208@users.noreply.github.com>
2022-12-28 00:15:15 +0200

    added 2nd task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
-console.log(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" 
+        href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        />
+        </a>
+        `;
+        })
+        .join('');
+}
+
+let newGallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionSelector: 'img',
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: '250ms',
+});
+newGallery.on('show.simplelightbox', {});
